fdb5fa6ad79fcb8b2424a7ce6547f62d9fb91d89
李庆彬 <13681551812@163.com>
2016-03-20 12:16:07 +0800

    Initial Commit

---
+++ Sina/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sina
+//
+//  Created by 李庆彬 on 3/20/16.
+//  Copyright © 2016 LQB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SinaTests/SinaTests.swift
@@ -0,0 +1,36 @@
+//
+//  SinaTests.swift
+//  SinaTests
+//
+//  Created by 李庆彬 on 3/20/16.
+//  Copyright © 2016 LQB. All rights reserved.
+//
+
+import XCTest
+@testable import Sina
+
+class SinaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
